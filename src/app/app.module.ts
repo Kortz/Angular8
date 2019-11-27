d573c7255ff866ff1628ae27afa2d6af4ed287bd
Kortz <hampus.helmbrant.persson@hotmail.se>
2019-11-27 14:42:21 +0000

    Assignment 1: Revert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component.ts';
 import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent, ServerComponent, ServersComponent, WarningAlertComponent, SuccessAlertComponent ],
+  declarations: [ AppComponent, ServerComponent, ServersComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
