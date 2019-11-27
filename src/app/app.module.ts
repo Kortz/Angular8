b0ed6123246d4a149fed863c9645f786a7d677b7
Kortz <hampus.helmbrant.persson@hotmail.se>
2019-11-27 14:41:18 +0000

    Assignment 1: Practicing Components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component.ts';
 import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent, ServerComponent, ServersComponent ],
+  declarations: [ AppComponent, ServerComponent, ServersComponent, WarningAlertComponent, SuccessAlertComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
