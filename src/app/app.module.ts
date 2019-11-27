47422cd82028e4c1260a7c558a6fccc2013ec825
Kortz <hampus.helmbrant.persson@hotmail.se>
2019-11-27 14:01:49 +0000

    First custom component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component.ts';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, ServerComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
