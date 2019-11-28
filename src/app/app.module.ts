e9cd4c810724ca637fbfa0238de7db1a52f4de4d
Kortz <hampus.helmbrant.persson@hotmail.se>
2019-11-28 10:35:56 +0000

    Two-way databinding.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component.ts';
 import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, ServerComponent, ServersComponent ],
   bootstrap:    [ AppComponent ]
 })
