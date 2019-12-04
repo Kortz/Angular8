d474b6ffda8f3d897ba5e252a82221e03520dfef
Kortz <hampus.helmbrant.persson@hotmail.se>
2019-12-04 13:40:02 +0000

    Directives, HostBinding, and HostListeners.
    Added HostBinding to bind style instead of using renderer.

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,9 +1,15 @@
-import { ElementRef, Renderer2, OnInit, Directive, HostListener } from '@angular/core';
+import { ElementRef, Renderer2, OnInit, Directive, HostListener, HostBinding} from '@angular/core';
 
 @Directive({
     selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+    @HostBinding('style.backgroundColor')
+    backgroundColor: string = 'transparent';
+
+    @HostBinding('style.color')
+    fontColor: string = 'black';
+
     constructor(private element: ElementRef, private renderer: Renderer2) {}
 
     ngOnInit() {
@@ -13,13 +19,17 @@ export class BetterHighlightDirective implements OnInit {
 
     @HostListener('mouseover')
     mouseHover() {
-        this.renderer.setStyle(this.element.nativeElement, 'background-color', 'black');
-        this.renderer.setStyle(this.element.nativeElement, 'color', 'white');
+        // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'black');
+        // this.renderer.setStyle(this.element.nativeElement, 'color', 'white');
+        this.backgroundColor = 'black';
+        this.fontColor = 'white';
     }
 
     @HostListener('mouseout')
     mouseLeave() {
-        this.renderer.setStyle(this.element.nativeElement, 'background-color', 'white');
-        this.renderer.setStyle(this.element.nativeElement, 'color', 'black');
+        // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'white');
+        // this.renderer.setStyle(this.element.nativeElement, 'color', 'black');
+        this.backgroundColor = 'transparent';
+        this.fontColor = 'black';
     }
 }
