5bd79952ea59e8174f431718a6af53c066da38b5
Kortz <hampus.helmbrant.persson@hotmail.se>
2019-12-04 13:30:41 +0000

    Directives, HostListener, and DOM events.
    Added HostListener to directive to listen for mouseover and mouseout DOM events.

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,13 +1,25 @@
-import { ElementRef, Renderer2, OnInit, Directive } from '@angular/core';
+import { ElementRef, Renderer2, OnInit, Directive, HostListener } from '@angular/core';
 
 @Directive({
     selector: '[appBetterHighlight]'
 })
-export class BetterHighlightDirective implements OnInit{
+export class BetterHighlightDirective implements OnInit {
     constructor(private element: ElementRef, private renderer: Renderer2) {}
 
     ngOnInit() {
+        // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'black');
+        // this.renderer.setStyle(this.element.nativeElement, 'color', 'white');
+    }
+
+    @HostListener('mouseover')
+    mouseHover() {
         this.renderer.setStyle(this.element.nativeElement, 'background-color', 'black');
         this.renderer.setStyle(this.element.nativeElement, 'color', 'white');
+    }
+
+    @HostListener('mouseout')
+    mouseLeave() {
+        this.renderer.setStyle(this.element.nativeElement, 'background-color', 'white');
+        this.renderer.setStyle(this.element.nativeElement, 'color', 'black');
     }
 }
