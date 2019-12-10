06ce5e2da2dc882d75f1b9fb9b53e14097ff4346
Kortz <kortspelaren@hotmail.com>
2019-12-10 14:01:59 +0000

    Services, Injectables, and  basic Observable emit subscribe.

---
+++ src/app/shared/accounts.service.ts
@@ -0,0 +1,34 @@
+import { LoggingService } from './logging.service';
+import { Injectable, EventEmitter } from '@angular/core';
+
+@Injectable()
+export class AccountsService {
+    accounts = [
+        {
+          name: 'Master Account',
+          status: 'active'
+        },
+        {
+          name: 'Testaccount',
+          status: 'inactive'
+        },
+        {
+          name: 'Hidden Account',
+          status: 'unknown'
+        }
+    ];
+
+    statusUpdated = new EventEmitter<string>();
+
+    constructor(private loggingService: LoggingService) {}
+
+    addAccount(newName: string, newStatus: string) {
+        this.accounts.push({name: newName, status: newStatus});
+        this.loggingService.logStatusChange(newStatus);
+    }
+
+    updateStatus(id: number, status: string) {
+        this.accounts[id].status = status;
+        this.loggingService.logStatusChange(status);
+    }
+}
