2aa49538b3346c27a4b35388f09d40ec983e1535
Kortz <kortspelaren@hotmail.com>
2019-12-13 09:14:05 +0000

    Subscribing to query params, and logging result.

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -11,13 +12,29 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  allowEdit: string;
+  fragment: string;
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
+
+    console.log('Snapshot: ' + this.route.snapshot.queryParams);
+    console.log('Snapshot: ' + this.route.snapshot.fragment);
+
+    this.route.queryParams.subscribe(
+      (p: Params) => {
+        console.log('Subscribe: ' + p);
+      }
+    );
+    this.route.fragment.subscribe(
+      (fragment: string) => {
+        console.log('Subscribe: ' + fragment);
+      }
+    );
   }
 
   onUpdateServer() {
