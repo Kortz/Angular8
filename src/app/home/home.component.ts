870a7d8e8e335fb201b3c28b5571890fb8bbc23b
Kortz <kortspelaren@hotmail.com>
2019-12-17 13:57:11 +0000

    Pipes, operators, and chaining operators.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { interval, Subscription, Observable } from 'rxjs';
+import { interval, Subscription, Observable} from 'rxjs';
+import { map, filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -21,7 +22,7 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
-        if (count == 5) {
+        if (count === 5) {
           observer.complete();
         }
         if (count > 3) {
@@ -31,7 +32,17 @@ export class HomeComponent implements OnInit, OnDestroy {
       }, 1000);
     });
 
-    this.count = customInternalObservable.subscribe({
+    //Operator
+    const pipedObservable: Observable<any> = customInternalObservable.pipe(
+      filter((data: number) => {
+        return data % 2 !== 0 ? true : false;
+      }),
+      map((data: number) => {
+        return 'Round: ' + (+data + 1);
+      })
+    );
+
+    this.count = pipedObservable.subscribe({
       next(data) {console.log(data); },
       error(error) {console.log(error); },
       complete() {console.log('Observer completed!'); }
