36c76341de60214d06b87566ea3961a8f8371800
Kortz <kortspelaren@hotmail.com>
2020-01-09 15:12:38 +0000

    Clean up interceptors with proper indentation / simplify.

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -7,13 +7,7 @@ export class AuthInterceptorService implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const modifiedRequest = req.clone({headers: req.headers.append('modifiedHeader', 'val1')});
-        console.log('Intercepted request');
-        return next.handle(modifiedRequest)
-            .pipe(
-                tap(event => {
-                    console.log('Logging response');
-                })
-            );
+        return next.handle(modifiedRequest);
     }
 
 }
--- src/app/logging-interceptor.service.ts
+++ src/app/logging-interceptor.service.ts
@@ -5,9 +5,11 @@ import { tap } from 'rxjs/operators';
 export class LoggingInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log(req);
-        return next.handle(req).pipe(tap(event => {
-            console.log(event);
-        }));
+        return next.handle(req)
+            .pipe(
+                tap(event => {
+                    console.log(event);
+                }));
     }
 
 }
