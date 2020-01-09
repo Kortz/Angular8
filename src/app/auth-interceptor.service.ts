228bc498deff9a139eeb4fe9d1f5892ad319decc
Kortz <kortspelaren@hotmail.com>
2020-01-09 14:56:57 +0000

    Modify outgoing request and add custom header using interceptor.

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -5,8 +5,9 @@ import { Observable } from 'rxjs';
 export class AuthInterceptorService implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const modifiedRequest = req.clone({headers: req.headers.append('modifiedHeader', 'val1')});
         console.log('Intercepted request');
-        return next.handle(req);
+        return next.handle(modifiedRequest);
     }
 
 }
