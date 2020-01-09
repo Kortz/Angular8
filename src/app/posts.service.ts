258fc3f2b8c89b5c2ba86f561627f9065b51d07b
Kortz <kortspelaren@hotmail.com>
2020-01-09 12:59:25 +0000

    Moving http code to separate service.

---
+++ src/app/posts.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Post } from './post.model';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PostService {
+
+    constructor(private httpClient: HttpClient) {}
+
+    storePost(postData: Post) {
+        // Send Http request
+        this.httpClient
+            .post<{ name: string }>('https://angular-7ca7d.firebaseio.com/posts.json', postData)
+            .subscribe(responseData => {
+                console.log(responseData);
+            });
+    }
+
+    fetchAllPosts() {
+        return this.httpClient
+            .get<{ [key: string]: Post }>('https://angular-7ca7d.firebaseio.com/posts.json')
+            .pipe(
+                map(responseData => {
+                const postArray: Post[] = [];
+                for (const key in responseData) {
+                    if (responseData.hasOwnProperty(key)) {
+                    postArray.push({ ...responseData[key], id: key});
+                    }
+                }
+                return postArray;
+                }));
+            //     .subscribe(responseData => {
+            //     this.loadedPosts = responseData;
+            // });
+    }
+}
