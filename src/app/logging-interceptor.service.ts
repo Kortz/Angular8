4eb70841b429bbe17690b4e01844a9760f6f0634
Kortz <kortspelaren@hotmail.com>
2020-01-09 15:04:07 +0000

    Add new basic logging interceptor.

---
+++ src/app/logging-interceptor.service.ts
@@ -0,0 +1,9 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from "@angular/common/http";
+import { Observable } from 'rxjs';
+
+export class LoggingInterceptorService implements HttpInterceptor {
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        throw new Error("Method not implemented.");
+    }
+
+}
