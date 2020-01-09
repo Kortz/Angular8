1dd5956fde17de927a6d9c4fd1bff4fbdaeb8116
Kortz <kortspelaren@hotmail.com>
2020-01-09 14:51:24 +0000

    Implemented intercept method of Interceptor.

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,11 +1,12 @@
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { Observable } from 'rxjs';
 
 
 export class AuthInterceptorService implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        throw new Error("Method not implemented.");
+        console.log('Intercepted request');
+        return next.handle(req);
     }
 
 }
