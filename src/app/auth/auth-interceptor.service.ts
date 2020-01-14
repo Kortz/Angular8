cb41ef24583ca24372b7514f856d521860c3ccb5
Kortz <kortspelaren@hotmail.com>
2020-01-14 11:00:45 +0000

    Lint fixes

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpParams } from '@angular/common/http'
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { AuthService } from './auth.service';
@@ -14,7 +14,7 @@ export class AuthInterceptorService implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return this.authService.userChanged.pipe(take(1), exhaustMap(user => {
-            const modifiedRequest = 
+            const modifiedRequest =
                 req.clone(
                     { params: new HttpParams()
                         .set('auth', user != null ? user.getTokenId() : null)});
