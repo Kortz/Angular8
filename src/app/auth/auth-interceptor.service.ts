9fc13a4ce1c0d855169d8ffb0d6fe232de28d013
Kortz <kortspelaren@hotmail.com>
2020-01-14 09:49:00 +0000

    AuthInterceptor implemented to add auth token to all outgoing requests.

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -2,12 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http'
 import { Observable } from 'rxjs';
 
+import { AuthService } from './auth.service';
+import { exhaustMap, take } from 'rxjs/operators';
+
 @Injectable({
     providedIn: 'root'
 })
 export class AuthInterceptor implements HttpInterceptor {
+
+    constructor(private authService: AuthService) {}
+
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        throw new Error("Method not implemented.");
+        return this.authService.userChanged.pipe(take(1), exhaustMap(user => {
+            req.headers.append('auth', user != null ? user.getTokenId() : null);
+            return next.handle(req);
+        }));
     }
 
 }
