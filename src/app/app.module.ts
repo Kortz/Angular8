3d3b52592981bdf992df9df1a761c9fbb46a7b68
Kortz <kortspelaren@hotmail.com>
2020-01-14 14:26:41 +0000

    Moved declarations from app module to recipes module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,11 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipeBookComponent } from './recipe-book/recipe-book.component';
-import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipe-book/recipe-list/recipe/recipe-detail/recipe-detail.component';
 import { ShoppingCartManagerComponent } from './shopping-cart/shopping-cart-manager/shopping-cart-manager.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
-import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { CartService } from './shared/cart.service';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeStartComponent } from './recipe-book/recipe-list/recipe/recipe-start/recipe-start.component';
-import { EditRecipeComponent } from './recipe-book/recipe-list/recipe/edit-recipe/edit-recipe.component';
 import { RecipesService } from './shared/recipes.service';
 import { DataStorageService } from './shared/data-storage.service';
 import { AuthComponent } from './auth/auth.component';
@@ -23,19 +17,37 @@ import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinne
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { AlertComponent } from './shared/alert/alert.component';
 import { PlaceholderDirective } from './shared/placeholder.directive';
+import { RecipesModule } from './recipe-book/recipes.module';
 
 @NgModule({
-  imports:      [ BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule, HttpClientModule ],
-  declarations: [ AppComponent, HeaderComponent, RecipeBookComponent, DropdownDirective,
-    RecipeListComponent, RecipeDetailComponent, ShoppingCartManagerComponent,
-    ShoppingCartComponent, RecipeItemComponent, RecipeStartComponent,
-    EditRecipeComponent, AuthComponent, LoadingSpinnerComponent, AlertComponent, PlaceholderDirective],
-  bootstrap:    [ AppComponent ],
+  imports:      [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RecipesModule
+  ],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    DropdownDirective,
+    ShoppingCartManagerComponent,
+    ShoppingCartComponent,
+    AuthComponent,
+    LoadingSpinnerComponent,
+    AlertComponent,
+    PlaceholderDirective
+  ],
+  bootstrap: [
+    AppComponent
+  ],
   providers: [
     CartService,
     RecipesService,
     DataStorageService,
-    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true} ],
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}
+  ],
   entryComponents: [
     AlertComponent
   ]
--- src/app/recipe-book/recipes.module.ts
+++ src/app/recipe-book/recipes.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 
+import { RecipeBookComponent } from './recipe-book.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-list/recipe/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipeStartComponent } from './recipe-list/recipe/recipe-start/recipe-start.component';
+import { EditRecipeComponent } from './recipe-list/recipe/edit-recipe/edit-recipe.component';
+
 @NgModule({
     imports: [
-
-     ],
+        RecipeBookComponent,
+        RecipeListComponent,
+        RecipeDetailComponent,
+        RecipeItemComponent,
+        RecipeStartComponent,
+        EditRecipeComponent
+    ],
     declarations: [ ],
     bootstrap: [ ],
     providers: [ ],
