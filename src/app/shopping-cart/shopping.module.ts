c7c0c0704dc3ead130bbfd88921d69d586db9199
Kortz <kortspelaren@hotmail.com>
2020-01-14 15:10:09 +0000

    Fix incorrect router module import on submodule.

--- src/app/shopping-cart/shopping.module.ts
+++ src/app/shopping-cart/shopping.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from '../app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { ShoppingCartManagerComponent } from './shopping-cart-manager/shopping-cart-manager.component';
 import { ShoppingCartComponent } from './shopping-cart.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     imports: [
-        AppRoutingModule,
+        RouterModule,
         FormsModule,
         BrowserModule,
     ],
