91403bacb9d104b8865ff2fae1e70fa1e041d158
Kortz <kortspelaren@hotmail.com>
2020-01-14 15:24:38 +0000

    Remove unused imports.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,6 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { RecipeBookComponent } from './recipe-book/recipe-book.component';
-import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
-import { RecipeDetailComponent } from './recipe-book/recipe-list/recipe/recipe-detail/recipe-detail.component';
-import { RecipeStartComponent } from './recipe-book/recipe-list/recipe/recipe-start/recipe-start.component';
-import { EditRecipeComponent } from './recipe-book/recipe-list/recipe/edit-recipe/edit-recipe.component';
-import { RecipesResolverService } from './shared/recipes-resolver.service';
 import { AuthComponent } from './auth/auth.component';
-import { AuthGuardService } from './auth/auth.guard';
 
 const routes: Routes = [
     {path: '', redirectTo: '/auth', pathMatch: 'full'},
