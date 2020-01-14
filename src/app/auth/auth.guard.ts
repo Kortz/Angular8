db33ea4b74c7942405e0b3925235a58564a34049
Kortz <kortspelaren@hotmail.com>
2020-01-14 12:57:20 +0000

    Redirect from guard using basic tap if not authenticated.

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,22 +1,26 @@
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router'
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router'
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { AuthService } from './auth.service';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthGuardService implements CanActivate {
 
-    constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService, private router: Router) {}
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
         boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
         return this.authService.userChanged.pipe(map(user => {
             const valid = user !== null;
             return valid;
+        }), tap(isAuthenticated => {
+            if (!isAuthenticated) {
+                this.router.navigate(['/auth']);
+            }
         }));
     }
 }
